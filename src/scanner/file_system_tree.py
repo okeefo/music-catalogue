af1110d6ce6537e32c104196690f44bd8fa94762
Keith Pyle <keith.pyle@gmail.com>
2024-01-09 21:53:05 +0000

    Preview Package files by label
    Stage one complete, Issues are:
    
    1 - files with the same label have individual dirs.
    2 - newly created dirs have wrong icon
    3 - files are still in original location they shoudl have moved.

--- src/scanner/file_system_tree.py
+++ src/scanner/file_system_tree.py
@@ -3,11 +3,15 @@ import os
 from PyQt5.QtWidgets import QApplication, QStyle
 from PyQt5.QtGui import QIcon
 from tinytag import TinyTag
+from mutagen.id3 import ID3
+import taglib
+
 
 # The class below is FileType. It is an enum that represents the type of a file. It has two values: DIRECTORY and FILE.
 class FsoType(Enum):
     DIRECTORY = "directory"
     FILE = "file"
+
 
 app = QApplication([])
 
@@ -18,54 +22,57 @@ fosIcons = {
     "mp3": QIcon(os.path.abspath("icons/mc-1-mp3.ico")),
     "wav": QIcon(os.path.abspath("icons/mc-1-wav.ico")),
     "jpg": QIcon(os.path.abspath("icons/mc-1-jpg.ico")),
-    "png": QIcon(os.path.abspath("icons/mc-1-png.ico"))
+    "png": QIcon(os.path.abspath("icons/mc-1-png.ico")),
 }
 
 supported_audio_extensions = ["mp3", "wav"]
 
+
 def get_icon(file_type, extension=None):
     if file_type == FsoType.DIRECTORY:
-        return fosIcons[file_type]
-    else:
+        return fosIcons[FsoType.DIRECTORY]
+    if extension in supported_audio_extensions:
         return fosIcons[extension]
+    else:
+        return fosIcons[file_type]
 
 
 class FsoNode:
-    
-    def __init__(self, absolute_path, parent=None):
+    def __init__(self, absolute_path, strong=None, typeOverride=None):
         self.absolute_path = absolute_path
         self.name = os.path.basename(absolute_path)
-        self.type = self._init_type()
+        self.type = self._init_type(typeOverride)
         self.extension = self._init_extension()
         self.icon = self._init_icon()
         self.children = []
-        self.parent = parent
-    
-    def _init_type(self):
-        return FsoType.DIRECTORY if os.path.isdir(self.absolute_path) else FsoType.FILE
-    
+
+    def _init_type(self, typeOverride=None):
+        if typeOverride is not None:
+            return typeOverride
+        else:
+            return (
+                FsoType.DIRECTORY if os.path.isdir(self.absolute_path) else FsoType.FILE
+            )
+
     def _init_extension(self):
         if self.type == FsoType.FILE:
             return os.path.splitext(self.absolute_path)[1][1:]
         else:
             return None
-    
+
     def _init_icon(self):
         if self.type == FsoType.DIRECTORY:
             return get_icon(self.type)
         else:
             return get_icon(self.type, self.extension)
-    
+
     def copy(self):
         copied_node = FsoNode(self.absolute_path)
         copied_node.children = [child.copy() for child in self.children]
         return copied_node
 
-    def add_child_node(self, name, file_type=None, extension=None):
-        if file_type is None:
-            self.children.append(name)  # Assuming 'name' is a FsoNode object
-        else:
-            self.children.append(FsoNode(name, file_type, get_icon(file_type, extension), extension))
+    def add_child_node(self, node):
+        self.children.append(node)
 
     def add_child_nodes(self, nodes):
         self.children.extend(nodes)
@@ -83,68 +90,72 @@ class FsoNode:
         return next((child for child in self.children if child.type == file_type), None)
 
     def get_child_node_by_extension(self, extension):
-        return next((child for child in self.children if child.extension == extension), None)
+        return next(
+            (child for child in self.children if child.extension == extension), None
+        )
 
     def get_child_nodes_by_extension(self, extension):
-        return [child for child in self.children if child.extension == extension] or None
+        return [
+            child for child in self.children if child.extension == extension
+        ] or None
 
     def get_child_nodes_by_extension_and_type(self, extension, file_type):
-        return [child for child in self.children if child.extension == extension and child.type == file_type] or None
+        return [
+            child
+            for child in self.children
+            if child.extension == extension and child.type == file_type
+        ] or None
 
     def get_child_node_by_extension_and_type(self, extension, file_type):
-        return next((child for child in self.children if child.extension == extension and child.type == file_type),
-                    None)
+        return next(
+            (
+                child
+                for child in self.children
+                if child.extension == extension and child.type == file_type
+            ),
+            None,
+        )
 
     def __repr__(self):
         return f"FsoNode({self.name}, {self.type}, {self.icon}, {self.absolute_path}, {self.extension}, {self.children})"
 
     def __eq__(self, other):
-            if isinstance(other, self.__class__):
-                return (
-                    self.absolute_path == other.absolute_path and
-                    self.name == other.name and
-                    self.type == other.type and
-                    self.icon == other.icon and
-                    self.extension == other.extension and
-                    self.children == other.children
-                )
-            return False
-    
+        if isinstance(other, self.__class__):
+            return (
+                self.absolute_path == other.absolute_path
+                and self.name == other.name
+                and self.type == other.type
+                and self.icon == other.icon
+                and self.extension == other.extension
+                and self.children == other.children
+            )
+        return False
 
-    def get_children(self): 
+    def get_children(self):
         if self.type == FsoType.DIRECTORY:
             return os.listdir(self.absolute_path)
         else:
             return None
-        
-    def get_parent(self):
-        if self.type == FsoType.DIRECTORY:
-            return os.path.dirname(self.absolute_path)
-        else:
-            return None
-    
+
     def get_absolute_path(self):
-        return self.absolute_path  
-    
+        return self.absolute_path
+
     def get_name(self):
         return self.name
-    
+
     def get_type(self):
         return self.type
-    
+
     def get_icon(self):
         return self.icon
-    
+
     def get_extension(self):
         return self.extension
-       
-    def get_children(self):
-        return self.children 
-    
-    def get_parent(self):
-        return self.parent
 
-    def get_id3_tag(self, tag):
+    def get_children(self):
+        return self.children
+
+    def get_id3_tag(self, tag_name_requested):
         """
         This function returns the requested ID3 tag if the file is a supported audio file.
         It checks if the file extension is in the list of supported audio extensions.
@@ -153,6 +164,10 @@ class FsoNode:
         if self.type != FsoType.FILE:
             return None
         if self.extension in supported_audio_extensions:
-            tags = TinyTag.get(self.absolute_path)
-            return getattr(tags, tag) 
-
+            song = taglib.File(self.absolute_path)
+            print(song.tags)
+            return (
+                song.tags[tag_name_requested][0]
+                if (song.tags.get(tag_name_requested))
+                else None
+            )
--- src/scanner/repackage_dir.py
+++ src/scanner/repackage_dir.py
@@ -1,5 +1,7 @@
+import os
 from tinytag import TinyTag
 from scanner import scanner_dir
+from scanner.file_system_tree import FsoNode, FsoType
 
 
 def preview_repackage(tree_structure_source, tree_structure_target, update_statusbar):
@@ -15,18 +17,20 @@ def preview_repackage(tree_structure_source, tree_structure_target, update_statu
 
     # now lets loop through the source tree and look for files to move
     for child in tree_structure_source.children:
-        if child.type == scanner_dir.FileType.FILE:
-            # we have a file, so lets see if we have a publisher
-            if publisher := get_publisher(child.name):
-                new_tree.add_child_node(publisher, scanner_dir.FileType.DIRECTORY)
+        if child.type == FsoType.FILE:
+            # we have a file, so lets see if we have a Label tag
+            if label := child.get_id3_tag("LABEL"):
+                
+                path = child.absolute_path
+                file_name = os.path.basename(path)  
+                new_dir = path.replace(file_name, label)
+                new_path = os.path.join(new_dir, file_name)
+                node = FsoNode(new_dir, FsoType.DIRECTORY)
+                node.add_child_node(FsoNode(new_path, FsoType.FILE))
+                new_tree.add_child_node(node)
             else:
                 # we don't have a publisher, so let's create a new child node in the target tree called "Unknown Publisher"
-                new_tree.add_child_node("Unknown Publisher", scanner_dir.FileType.DIRECTORY)
+                new_tree.add_child_node("Unknown Publisher", FsoType.DIRECTORY)
 
     update_statusbar("Repackaging... Done")
     return new_tree
-
-
-def get_publisher(file_path):
-    tag = TinyTag.get(file_path)
-    return tag.publisher
